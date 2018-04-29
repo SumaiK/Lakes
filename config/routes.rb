408f6f84323e71a023e9136c62fba447a9dd941f
Sumai Khayat <sumaikhayat@gmail.com>
2018-04-29 13:48:33 +0100

    user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+    # as :user do
+    #   get 'signin', to: 'pages#home' as :new_user_sessions
+    #   post 'signin', to: 'pages#home' as :user_session
+    #   delete 'signout', to: 'devise/session#destroy' as :destroy_user_session
+    # end
+
   root to: 'pages#home'
+  resources :user, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
