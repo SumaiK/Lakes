367d9faa89122683ea752367b99d53a8981b3665
Sumai Khayat <sumaikhayat@gmail.com>
2018-05-02 21:23:38 +0100

    started holi album controller

---
+++ app/controllers/holiday_album_controller.rb
@@ -0,0 +1,48 @@
+class HolidayAlbumController < ApplicationController
+  def new
+    @holiday_album = HolidayAlbum.new
+    @holiday = Holiday.find(params[:holiday_id])
+    authorize @holiday_album
+  end
+
+  def create
+    @holiday_album = HolidayAlbum.new(holiday_album_params)
+    @holiday = Holiday.find(params[:holiday_id])
+    if @holiday_album.save
+      redirect_to holidays_album_path(@holiday_album)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @holiday_album = HolidayAlbum.find(params[:id])
+    @holiday = Holiday.find(params[:holiday_id])
+    authorize @holiday_album
+  end
+
+  def update
+    @holiday_album = HolidayAlbum.find(params[:id])
+    @holiday = Holiday.find(params[:holiday_id])
+    @holiday_album.update(holiday_album_params)
+    if @holiday_album.save
+      redirect_to holiday_album_path(@holiday_album)
+    else
+      render :new
+    end
+    authorize @holiday_album
+  end
+
+  def destroy
+    @holiday_album = HolidayAlbum.find(params[:id])
+    @holiday_album.destroy
+    redirect_to holiday_album_path(@holiday_album)
+    authorize @holiday_album
+  end
+
+  private
+
+  def holiday_album_params
+    params.require(:holiday_album).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :users, only: [:index, :show, :edit, :update]
-  resources :holidays
+  resources :holidays do
+    resources :holiday_invites, only: [:new, :create]
+    resources :holiday_albums, only: [:new, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
